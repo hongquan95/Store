0e8ffb096ad1daf1d3169a221ac244b471e4141d
hongquan95 <hongquan95.tran@gmail.com>
2018-12-20 23:26:43 +0700

    add product and category factory, seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Product::class, function (Faker $faker) {
+    return [
+        'name' => 'Product ' . random_int(100, 200),
+        'supplier_id' => factory(App\Models\Supplier::class)->create()->id,
+        'price' => $faker->randomElement([1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]),
+        'rating' => $faker->numberBetween(1, 5),
+        'description' => $faker->text(50),
+    ];
+});
