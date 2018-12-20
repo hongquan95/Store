205c2c379a56142cc05df01065e6e712d12fe403
hongquan95 <hongquan95.tran@gmail.com>
2018-12-20 10:12:16 +0700

    fix factory

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Models\Customer::class, function (Faker $faker) {
     $faker = \Faker\Factory::create('vi_VN');
     return [
         'name' => $faker->name,
-        'address' => $faker->address,
+        'address' => preg_replace('~[\r\n]+~', '', $faker->address),
         'phone' => $faker->phoneNumber,
         'email' => $faker->safeEmail,
         'gender' => random_int(1, 3),
--- database/factories/SupplierFactory.php
+++ database/factories/SupplierFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Models\Supplier::class, function (Faker $faker) {
     $faker = \Faker\Factory::create('vi_VN');
     return [
         'name' => $faker->name,
-        'address' => $faker->address,
+        'address' => preg_replace('~[\r\n]+~', '', $faker->address),
         'phone' => $faker->phoneNumber,
         'email' => $faker->safeEmail,
     ];
