995f5873fb3334f1be0ab8c4e0b0c04c72e72d31
hongquan95 <hongquan95.tran@gmail.com>
2019-01-01 01:25:38 +0700

    fix update product request

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Category;
 use App\Http\Requests\JsonRequest;
 use App\Http\Services\ProductService;
 
@@ -29,6 +30,16 @@ class UpdateProductRequest extends JsonRequest
             'name' => 'required|unique:products,name,' . $this->id,
             'price' => 'required|numeric|min:0',
             'description' => 'nullable',
+            'category_ids' => 'required|array',
+            'category_ids' => function($attributes, $params, $fail) {
+                $ids = Category::pluck('id')->toArray();
+                foreach ($params as $param) {
+                    if (!in_array($param, $ids)) {
+                        $fail('The Category atrribute is wrong!');
+                        return;
+                    }
+                }
+            },
         ];
     }
 }
